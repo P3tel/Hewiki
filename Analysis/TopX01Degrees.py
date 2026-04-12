8ef2683f13185d7236b6e70679551b3e1d957e71
Ori Bahat-Petel <oribahatpetel@gmail.com>
2026-04-12 22:06:58 +0300

    Change top nodes display from 10 to 50

--- Analysis/TopX01Degrees.py
+++ Analysis/TopX01Degrees.py
@@ -24,13 +24,13 @@ print("Nodes with out-degree = 0:", num_out0)
 # get names
 names = g.vs["title"]
 
-# top 10 in-degree nodes
+# top 50 in-degree nodes
 top_in_idx = sorted(range(len(in_deg)), key=lambda i: in_deg[i], reverse=True)[:50]
 print("\nTop 50 highest in-degree:")
 for i in top_in_idx:
     print(names[i], in_deg[i])
 
-# top 10 out-degree nodes
+# top 50 out-degree nodes
 top_out_idx = sorted(range(len(out_deg)), key=lambda i: out_deg[i], reverse=True)[:50]
 print("\nTop 50 highest out-degree:")
 for i in top_out_idx:
